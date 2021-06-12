141fc7c00db65bcbff1f21b0df6893a1ccc4eff5
Mike Rinehart <t0astt93@gmail.com>
2021-06-12 17:17:26 -0500

    Chapter 2

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+
+        // Let declares a variable, mut makes it mutable
+        // new is an associated function
+        let mut guess = String::new();
+    
+        // & denotes that an argument is a reference - avoiding copying data into memory multiple times
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
